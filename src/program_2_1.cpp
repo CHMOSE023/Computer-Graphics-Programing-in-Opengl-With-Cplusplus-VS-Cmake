eebdb0d15082b5be9d4304c2386ec4d7fd82ffed
Francisco Zavala <133605123+Nobody-1321@users.noreply.github.com>
2024-07-24 23:46:54 -0600

    Develop (#11)
    * I added soil2 inside the project so that whoever downloads the repository
    doesn't have to install soild2.
    I need to work on a clean and understandable syntax.
    
    * soil2 library dependency removed
    
    * documented program_2_1

--- src/program_2_1.cpp
+++ src/program_2_1.cpp
@@ -1,7 +1,22 @@
+//////////////////////////////////////////////////////////////////////
+//  Program 2.1                                                     //
+//  This program is a basic OpenGl application using GLFW           //
+//  for window management and GLAD for loading OpenGL functions.    //
+//  The program creates a window and clears the window with a       //
+//  red color.                                                      //
+//                                                                  //      
+//  you can find more information about how                         //
+//  this program works in the wiki of this repository.              //
+//                                                                  //      
+//  Francisco Zavala                                                //
+//  24/07/2024                                                      //
+//////////////////////////////////////////////////////////////////////
+
+
 // Include necessary libraries
 #include <glad/glad.h>  // Library for handling the loading of OpenGL functions
 #include <GLFW/glfw3.h> // Library for handling window and user input
-#include <iostream>     // Library for input/output operations
+#include <iostream>     
 
 // Initialization function (currently empty)
 void init(GLFWwindow* window){
@@ -9,25 +24,36 @@ void init(GLFWwindow* window){
 
 // Function to update the display
 void display(GLFWwindow* window){
-    glClearColor(1.0f,0.0f,0.0f,1.0f); // Set the background color
-    glClear(GL_COLOR_BUFFER_BIT);       // Clear the color buffer with the background color
+    glClear(GL_COLOR_BUFFER_BIT);       
+    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 }
 
-// Main function
 int main() {
     
     // Initialize GLFW, terminate program if failed
     if (!glfwInit()) {exit(EXIT_FAILURE);}
 
     // Set window properties
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);                  // Major OpenGL version
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);                  // Minor OpenGL version
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // OpenGL profile
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Forward compatibility
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);                  
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);                  
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            
 
-    // Create the window
-    GLFWwindow* window = glfwCreateWindow(1080, 720, "opengl_program_2_1", NULL, NULL);
-    // Terminate program if window creation failed
+    //Create the window
+    GLFWwindow* window = glfwCreateWindow(1080, 720, "program_2_1", NULL, NULL);
+    
+    //monitor properties
+    /*
+    const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+    GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, " program_2_1 ", nullptr, nullptr);    
+    std::cout << "width " <<mode->width <<std::endl;
+    std::cout << "height " <<mode->height <<std::endl;
+    std::cout << "refresh " << mode->refreshRate << std::endl;
+    std::cout << "blueBits " << mode->blueBits << std::endl;
+    std::cout << "redBits " << mode->redBits<< std::endl;
+    std::cout << "greenBits " << mode->greenBits << std::endl;
+    */   
+    
     if (!window) {
         glfwTerminate();
         return -1;
@@ -42,14 +68,19 @@ int main() {
         return -1;
     }
     
+
     // Main loop
     while (!glfwWindowShouldClose(window)) {
-        display(window);          // Update the display
-        glfwPollEvents();         // Process input events
-        glfwSwapBuffers(window);  // Swap the window buffers
+
+
+        display(window);          
+        glfwPollEvents();        
+        glfwSwapBuffers(window); 
     }
 
     // Terminate GLFW when program ends
+    glfwDestroyWindow(window);
     glfwTerminate();
+    
     return 0;
 }
